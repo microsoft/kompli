ad7a0f002e7d6f5f3028edf469e9415d62ea9f92
Krzysztof Kanas <kkanas@microsoft.com>
2026-06-11 11:02:14 +0200

    Telemetry: remove osconfig telemetry
    Signed-off-by: Krzysztof Kanas <kkanas@microsoft.com>

--- src/modules/complianceengine/src/lib/procedures/FileRegexMatch.cpp
+++ src/modules/complianceengine/src/lib/procedures/FileRegexMatch.cpp
@@ -6,7 +6,6 @@
 #include <ProcedureMap.h>
 #include <Regex.h>
 #include <Result.h>
-#include <Telemetry.h>
 #include <cerrno>
 #include <cstdlib>
 #include <dirent.h>
@@ -381,7 +380,6 @@ Result<Status> AuditFileRegexMatch(const FileRegexMatchParams& params, Indicator
     {
         int status = errno;
         OsConfigLogError(context.GetLogHandle(), "Failed to read directory '%s': %s", params.path.c_str(), strerror(status));
-        OSConfigTelemetryStatusTrace("readdir", status);
         return Error("Failed to read directory '" + params.path + "': " + strerror(status), status);
     }
 
